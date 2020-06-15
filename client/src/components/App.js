1b75e01770bcf2764f60ae54d0e381e5bb38c28b
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-06-15 12:01:00 -0700

    split html out

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Cart from "./Cart";
+import ProductList from "./ProductList";
+import AddProduct from "./AddProduct";
+
+/**
+ * Shop
+ *  - Cart
+ *  - Products
+ *   - EditableProduct
+ *     - Product
+ *     - EditProductForm
+ * - AddProduct
+ *   - AddProductForm
+ */
 
 const App = () => {
   return (
     <div id="app">
-      <h1>Welcome</h1>
+      <header>
+        <h1>The Shop!</h1>
+        <Cart />
+      </header>
+      <main>
+        <ProductList />
+        <AddProduct />
+      </main>
     </div>
   );
 };
