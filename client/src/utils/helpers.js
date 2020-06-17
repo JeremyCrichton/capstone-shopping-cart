b01bef5951bbca025c37be1cb1fbf4751a522bc2
edmondtam1 <edmondtam2@gmail.com>
2020-06-17 22:45:10 +0800

    Added sum

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,5 +1,5 @@
 export const sum = (arr) => {
   if (arr.length === 0) return "0.00";
-  const total = arr.reduce((total, curr) => total + curr);
-  console.log(total);
+  const total = arr.reduce((total, curr) => total + curr.quantity * curr.price, 0);
+  return Number(total).toFixed(2);
 }
